f04b6dc4570f40763ddb11bfbcd57ac3fe85114a
Denys Boiko <denys.boiko@gmail.com>
2021-03-05 19:42:01 +0200

    update assets

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by denisbojko on 02.03.17.
+ */
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './js/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
