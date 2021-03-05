74254a9ebc1e3f7c036f50fb42a362a5b9b22c87
Denys Boiko <denys.boiko@gmail.com>
2021-03-05 20:35:46 +0200

    change form handler

--- js/scripts.js
+++ js/scripts.js
@@ -362,7 +362,7 @@ $(function () {
 	/*
 		Validate Contact Form
 	*/
-	
+
 	$("#cform").validate({
 		ignore: ".ignore",
 		rules: {
@@ -386,25 +386,7 @@ $(function () {
 				}
 			}
 		},
-		success: "valid",
-		submitHandler: function() {
-			$.ajax({
-				url: 'mailer/feedback.php',
-				type: 'post',
-				dataType: 'json',
-				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
-				beforeSend: function() {
-				
-				},
-				complete: function() {
-				
-				},
-				success: function(data) {
-					$('#cform').fadeOut();
-					$('.alert-success').delay(1000).fadeIn();
-				}
-			});
-		}
+		success: "valid"
 	});
 	
 	
